de80926d19cdffc053f1c5f938d68bb8982995f0
yuwatida <yuwatida@yuwatidas-MacBook-Pro.local>
2020-09-03 00:44:21 -0400

    Make some change

--- Line.java
+++ Line.java
@@ -1,4 +1,16 @@
 public class Line2{ 
 
+	public static void main(String args[]){ 
+         
+         int num =0;
+
+         if(num > 5){
+           System.out.println("true");
+
+	     }else{ 
+
+		   System.out.println("false");
+	}
+
 
 }
